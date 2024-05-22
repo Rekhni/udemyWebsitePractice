7fe73fd9c44574556c6d013f7300301d18e48977
Rakhymzhan <mark.john.99@mail.ru>
2024-05-22 14:05:10 -0700

    setting up timer, scroll on modal

--- js/script.js
+++ js/script.js
@@ -110,13 +110,18 @@ window.addEventListener('DOMContentLoaded', () => {
           modalCloseBtn = document.querySelector('[data-close]');
 
 
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
     modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-            modal.classList.add('show');
-            modal.classList.remove('hide');
-            document.body.style.overflow = 'hidden';
-        })
+        btn.addEventListener('click', openModal)
     })
+
+
  
     function closeModal() {
         modal.classList.add('hide');
@@ -124,9 +129,7 @@ window.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = '';
     }
 
-    modalCloseBtn.addEventListener('click', () => {
-        closeModal();
-    })
+    modalCloseBtn.addEventListener('click', closeModal)
 
     modal.addEventListener('click', (e) => {
         if (e.target === modal) {
@@ -139,5 +142,17 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }
     })
+
+    const modalTimerId = setTimeout(openModal, 3000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.
+            documentElement.scrollHeight) {
+                openModal();
+                window.removeEventListener('scroll', showModalByScroll);
+            }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 })
 
