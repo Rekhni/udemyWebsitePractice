4a3ba7d73625544bae97c59f6b73948ca3a1297f
Rakhymzhan <mark.john.99@mail.ru>
2024-06-16 15:49:46 -0700

    installed babel and coreJS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
 import tabs from './modules/tabs';
 import modal from './modules/modal';
 import timer from './modules/timer';
@@ -6,6 +9,7 @@ import calc from './modules/calc';
 import forms from './modules/forms';
 import slider from './modules/slider';
 import { openModal } from './modules/modal';
+
 
 window.addEventListener('DOMContentLoaded', () => {
     
